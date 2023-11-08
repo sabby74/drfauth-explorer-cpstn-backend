9c1a5e515b4a7c3097f05780dce997b6c1f0f15e
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-08 17:14:11 -0500

    first commit

---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE drf_auth_explorer;
+CREATE USER drf_auth_exploreruser WITH PASSWORD 'drf_auth_explorer';
+GRANT ALL PRIVILEGES ON DATABASE drf_auth_explorer TO drf_auth_exploreruser;
