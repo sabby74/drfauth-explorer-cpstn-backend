37405a00cc3a743436de35899d28c7423ae0b912
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-14 19:42:49 -0500

    back to no=rmal

--- explorer_project/settings.py
+++ explorer_project/settings.py
@@ -146,10 +146,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://new-personnel-explorer.onrender.com"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
